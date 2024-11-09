2ffd5510b1265335dc5748de3d901b3e762785a4
Artem Nesin <artem.nesin@gmail.com>
2024-11-10 00:38:14 +0500

    locale calendar

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
-import './index.css'
-import { VueQueryPlugin } from '@tanstack/vue-query'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import './index.css';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
 
-createApp(App).use(router).use(createPinia()).use(VueQueryPlugin).mount('#app')
+dayjs.locale('ru');
+createApp(App).use(router).use(createPinia()).use(VueQueryPlugin).mount('#app');
